d6fa14f9ba99401ad84d6d577489578817f47f0d
Kevin Wang <wangkevin448@gmail.com>
2015-10-15 00:51:50 -0700

    now just need to view blogs

--- app/components/blogeditor/blogeditor.js
+++ app/components/blogeditor/blogeditor.js
@@ -6,11 +6,17 @@ let apiConfig = blogconfig.roboticsfirebase;
 
 class BlogeditorController {
   constructor($firebaseObject, $firebaseArray, $firebaseAuth) {
-    this.fbblogref = new Firebase(apiConfig.main);
+    this.fb = new Firebase(apiConfig.main);
 
-    this.postList = this.fbblogref.child(apiConfig.postList);
+    this.setYear = (year)=> {
+      this.blogyearsaved = year;
+      this.postListArray = this.fb.child(apiConfig.postList).child(year);
+      this.postListArrayIter = $firebaseArray(this.postListArray);
+      this.postsArray = this.fb.child(apiConfig.posts).child(year);
+      this.postsArrayIter = $firebaseArray(this.postsArray);
+    };
 
-    this.auth = $firebaseAuth(this.fbblogref);
+    this.auth = $firebaseAuth(this.fb);
 
     this.authuid = "Logged out";
 
@@ -20,16 +26,44 @@ class BlogeditorController {
       }).catch((error)=> {
         console.log("Authentication failed:", error);
       });
-    }
+    };
 
     this.logout = ()=> {
       this.authuid = "logged out";
       this.auth.$unauth();
-    }
+    };
 
-    this.post = (id, year, date, content)=> {
-      $firebaseArray(this.postList.child(year)).$add(year + id);
-    }
+    this.blogdate = moment().format('YYYY-MM-DD');
+
+    this.postSuccess = false;
+
+    this.post = (aId, aYear, aDate, aTitle, aContent)=> {
+      let id = aYear+aId;
+
+      let post = this.postsArray.push();
+      let idkey = post.key();
+      post.set({
+        id: id,
+        date: aDate,
+        title: aTitle,
+        content: aContent
+      });
+
+      let postListEntry = $firebaseObject(this.postListArray.child(id));
+      postListEntry.$value = {
+        id: id,
+        date: aDate,
+        title: aTitle,
+        idkey: idkey
+      };
+      postListEntry.$save();
+
+      this.blogid = '';
+      this.blogdate = '';
+      this.blogtitle = '';
+      this.blogcontent = '';
+      this.postSuccess = true;
+    };
   }
 }
 
