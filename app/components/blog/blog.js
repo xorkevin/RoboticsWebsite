a7554438b4f8162d1572e4d925b042d51b8f901b
Kevin Wang <wangkevin448@gmail.com>
2015-10-16 22:26:50 -0700

    blog view

--- app/components/blog/blog.js
+++ app/components/blog/blog.js
@@ -2,17 +2,35 @@ import {app} from '../../module.js';
 // import 'firebase'; causes error due to webpack use strict
 import {blogconfig} from '../../config.js';
 
-let apiConfig = blogconfig.roboticsfirebase;
 
 class BlogController {
   constructor() {
+    this.setPage = (pageid)=>{
+      if(pageid == 'p'){
+        this.api = blogconfig.roboticsfirebase;
+      } else if(pageid == 'r'){
+
+      } else if(pageid == 'a'){
+
+      } else if(pageid == 'o'){
+
+      } else if(pageid == 'g'){
+
+      }
+    };
+
     this.getYear = (year)=>{
-      this.blogList = new Firebase(apiConfig.main + '/' + apiConfig.postList);
+      this.year = year;
+      this.blogList = new Firebase(this.api.main + '/' + this.api.postList);
     };
 
     this.getPost = (year, key)=>{
 
     };
+
+  }
+
+  activate(){
 
   }
 }
